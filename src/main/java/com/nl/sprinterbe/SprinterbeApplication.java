dd8420f9e9358ab259de5ab90d5ca471ccd0db03
sobogil <69839941+sobogil@users.noreply.github.com>
2024-12-03 21:04:46 +0900

    first commit

---
+++ src/main/java/com/nl/sprinterbe/SprinterbeApplication.java
@@ -0,0 +1,13 @@
+package com.nl.sprinterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinterbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinterbeApplication.class, args);
+	}
+
+}
