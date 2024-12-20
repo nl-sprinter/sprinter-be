a232838549dccfeca05be61e97383e9171f793b5
sobogil <sotomas232@gmail.com>
2024-12-20 14:18:34 +0900

    Feat:'Entity 생성'     Like entity추가

--- src/main/java/com/nl/sprinterbe/entity/Like.java
+++ src/main/java/com/nl/sprinterbe/entity/Like.java
@@ -1,5 +1,28 @@
 package com.nl.sprinterbe.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "backlog_comment_id", nullable = false)
+    private BacklogComment backlogComment;
+
 }
