ee9d9c1ffc07866824a719f6c20c236ff365656e
sobogil <sotomas232@gmail.com>
2025-01-22 19:09:45 +0500

    Feat: ‘jsonLogin 구현’
    jsonLogin filter구현 완료

--- src/main/java/com/nl/sprinterbe/config/SecurityConfig.java
+++ src/main/java/com/nl/sprinterbe/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.nl.sprinterbe.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nl.sprinterbe.filter.CustomUsernamePasswordAuthenticationFilter;
+import com.nl.sprinterbe.handler.LoginFailureHandler;
+import com.nl.sprinterbe.handler.LoginSuccessHandler;
 import com.nl.sprinterbe.user.service.CustomOAuth2UserService;
 import com.nl.sprinterbe.user.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
@@ -8,23 +12,27 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity(debug =false)
+@EnableWebSecurity(debug =true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final CustomOAuth2UserService oAuth2UserService;
     private final CorsConfigurationSource corsConfigurationSource;
+    private final ObjectMapper objectMapper;
+    private final CustomUserDetailsService loginService;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,22 +41,23 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**","/api/v1/login", "/oauth2/**", "/login/**").permitAll()
-                        .requestMatchers("/api/v1/auth").permitAll()
+                        .requestMatchers("/api/v1/auth", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginProcessingUrl("/api/v1/login") // 로그인 URL
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                        .successHandler((request, response, authentication) -> {
-                            response.setStatus(200);
-                            response.getWriter().write("Login successful");
-                        })
-                        .failureHandler((request, response, exception) -> {
-                            response.setStatus(401);
-                            response.getWriter().write("Login failed: " + exception.getMessage());
-                        })
-                )
+                .addFilterBefore(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .formLogin(form -> form
+//                        .loginProcessingUrl("/api/v1/login") // 로그인 URL
+//                        .usernameParameter("email")
+//                        .passwordParameter("password")
+//                        .successHandler((request, response, authentication) -> {
+//                            response.setStatus(200);
+//                            response.getWriter().write("Login successful");
+//                        })
+//                        .failureHandler((request, response, exception) -> {
+//                            response.setStatus(401);
+//                            response.getWriter().write("Login failed: " + exception.getMessage());
+//                        })
+//                )
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(oAuth2UserService)
@@ -81,6 +90,38 @@ public class SecurityConfig {
     public WebSecurityCustomizer configureH2ConsoleEnable() {
         return web -> web.ignoring()
                 .requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    public CustomUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() throws Exception {
+        CustomUsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter = new CustomUsernamePasswordAuthenticationFilter(objectMapper);
+        usernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        usernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        usernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return usernamePasswordAuthenticationFilter;
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler();
+    }
+
+    /**
+     * 로그인 실패 시 호출되는 LoginFailureHandler 빈 등록
+     */
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+
+        return new ProviderManager(provider);
     }
 
 }
---
+++ src/main/java/com/nl/sprinterbe/handler/LoginSuccessHandler.java
@@ -0,0 +1,21 @@
+package com.nl.sprinterbe.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Slf4j
+@Component
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        System.out.println("LoginSuccessHandler.onAuthenticationSuccess");
+        response.setStatus(200);
+        response.getWriter().write("Login Success");
+    }
+}
