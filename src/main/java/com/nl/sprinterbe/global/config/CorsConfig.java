f7f840daef7e1eff638573c9c04e7295836335e1
kraftenty <kraftenty@gmail.com>
2025-04-10 00:59:58 +0900

    Feat: 0410 배포 - WebsocketConfig문제해결

--- src/main/java/com/nl/sprinterbe/global/config/CorsConfig.java
+++ src/main/java/com/nl/sprinterbe/global/config/CorsConfig.java
@@ -13,11 +13,13 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
-        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
--- src/main/java/com/nl/sprinterbe/global/config/WebSocketConfig.java
+++ src/main/java/com/nl/sprinterbe/global/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/v1/chat/ws")
-               .setAllowedOrigins("*")
+               .setAllowedOriginPatterns("*") // setAllowedOrigins() -> setAllowedOriginPatterns()
                .withSockJS();
     }
 
